3be5fc875066542dcfbe5cd5cf48fa26ead3bb47
Ondrej Spanik <spanik11@msn.com>
2025-01-01 04:07:03 +0100

    fix social subsite link

--- src/routes/[lang]/index.svelte
+++ src/routes/[lang]/index.svelte
@@ -445,7 +445,7 @@
 		nav={[
 						{icon: "fab fa-youtube", text: "Ayu Animations", href:"https://youtube.com/@ayu-animations/", isButton: true},
 						{icon: "fab fa-youtube", text: "iairu99", href:"https://youtube.com/@iairu99/"},
-						{icon: "fa fa-share", text: SK ? "Viac" : "More", href: SK ? "/sk/links" : "/en/links", modal: true}
+						{icon: "fa fa-share", text: SK ? "Viac" : "More", href: "/links", modal: true}
 		]}
 		/>
 	<Thumb
